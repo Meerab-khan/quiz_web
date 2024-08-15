f66324061a80a0b8ad382ab688ef0b75cb9dd232
Toseer Khan <toseerkhan@Shaheers-MacBook-Pro.local>
2024-08-16 00:05:35 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,161 @@
+let username = prompt(" Enter your name ?");
+let num = prompt("Enter your WMA number ");
+alert("Welcome__" + username + "__to this quiz session!");
+alert(" Good Luck !")
+ 
+// Quiz 1
+ let quiz1= prompt( " Inside which HTML element do we put the javascript?  A)<scripting> , B) <javascript> , C)<js> , D) <script> ") 
+ if (quiz1 === 'D'){
+    alert(" Weldone __! Your Answer is Correct____!!!");
+ }
+ 
+  else {
+    (quiz1 === "Anything else would be wrong");
+    alert("Incorrect__! Best of luck next time____!!!");
+ }
+
+
+//  Quiz 2
+let quiz2 = prompt("Where is the correct place to insert a JavaScript?   A)The <head> section , B)The <body> section , C) Both the <head> section and the <body> section are correct")
+if (quiz2 === 'C'){
+    alert(" Weldone __! Your Answer is Correct____!!!");
+}
+else{
+    (quiz2 === "Anything else would be wrong");
+    alert("Incorrect__! Best of luck next time____!!!");
+}
+
+
+//  Quiz 3
+let quiz3 = prompt("What is the correct syntax for referring to an external script called xxx.js?   A)<script src ='xxx.js'> ,  B)<script href ='xxx.js'> ,  C)<script name ='xxx.js'>    ");
+
+if (quiz === 'A'){
+    alert(" Weldone __! Your Answer is Correct____!!!");
+}
+else{
+    (quiz3 === "Anything else would be wrong");
+    alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Quiz 4
+let  quiz4= prompt("The external JavaScript file must contain the <script> tag. A)False , B)True");
+if (quiz4 === 'A'){
+    alert(" Weldone __! Your Answer is Correct____!!!");
+}
+else{
+    (quiz4 === "Anything else would be wrong");
+    alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Quiz 5
+let quiz5 = prompt("How do you write 'Hello World' in an alert box?  A)alertBox('HELLO WORLD');  ,B)msgBox('HELLO WORLD'); , C)msg('HELLO WORLD'); D)alert('HELLO WORLD');    ");
+
+if (quiz5 === 'D'){
+    alert(" Weldone __! Your Answer is Correct____!!!");
+}
+else{
+    (quiz5 === "Anything else would be wrong");
+    alert("Incorrect__! Best of luck next time____!!!");
+}
+
+
+//  Quiz 6
+let quiz6 = prompt("How do you create a function in JavaScript?  A) function:myFuncation() ,  B) function myFuncation() ,   C) function=myFuncation() " );
+if (quiz6 === 'B'){
+    alert(" Weldone __! Your Answer is Correct____!!!");
+}
+else{
+    (quiz6 === "Anything else would be wrong");
+    alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Quiz 7
+let quiz7 = prompt("How do you call a function named 'myFunction'?  A)myFuncation() , B)call funcation myFuncation(), C)call myFuncation()");
+if (quiz7 === 'A'){
+    alert(" Weldone __! Your Answer is Correct____!!!");
+}
+else{
+    (quiz7 === "Anything else would be wrong");
+    alert("Incorrect__! Best of luck next time____!!!");
+}
+
+
+//  Quiz8 
+let quiz8 = prompt("How does a WHILE loop start? A) while i=1 to 10 , B) while (i <= 10)  , C) while (i <= 10;i++)");
+if (quiz8 === 'B'){
+    alert(" Weldone __! Your Answer is Correct____!!!");
+}
+else{
+    (quiz8 === "Anything else would be wrong");
+    alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Quiz 9
+let quiz9 = prompt("How to write an IF statement in JavaScript? A)if i==5 then , B)if(i == 5)  , C)if i = 5 then, D)if i= 5 ");
+if (quiz9 === 'B'){
+    alert(" Weldone __! Your Answer is Correct____!!!");
+}
+else{
+    (quiz9 === "Anything else would be wrong");
+    alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Quiz 10
+let quiz10 = prompt(" How does a FOR loop start? A)for (i <= 5 ; i++) , B)for i 1 to 5 , C)for (i=0; i <= 5; i++) , D)for (i = 0 ; i <= 5)");
+if (quiz10 === 'D'){
+    alert(" Weldone __! Your Answer is Correct____!!!");
+}
+else{
+    (quiz10 === "Anything else would be wrong");
+    alert("Incorrect__! Best of luck next time____!!!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
